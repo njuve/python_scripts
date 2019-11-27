2df1c03a8ff37daa30ebdb31758565a4b3b7e296
njuve <ale.nnn.r@gmail.com>
2019-11-27 16:38:45 +0900

    Add plot_bar_with_pval_lines

--- python_scripts/core.py
+++ python_scripts/core.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import glob
 
 
@@ -23,3 +26,37 @@ def missing_info(df, title=None):
                             'ratio': ratio_missing
                         })
     return missing_table
+
+
+def _take_patch_info(patch):
+    return patch.get_height(), patch.get_x() + patch.get_width()/2
+
+
+def plot_bar_with_pval_lines(feature, data, signficant_pairs):
+    fig, ax = plt.subplots(1, 1)
+
+    g = sns.countplot(
+        feature,
+        data=data,
+        order=data[feature].value_counts().index,
+        ax=ax
+    )
+
+    # Remove right and top frame
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    patches = ax.patches
+    for pair in signficant_pairs:
+        patch1 = patches[pair[0]]
+        patch2 = patches[pair[1]]
+
+        height1, center1 = _take_patch_info(patch1)
+        height2, center2 = _take_patch_info(patch2)
+        max_height = max(height1, height2)
+
+        # Draw a signficant mark and a line like rotated "]"
+        ax.vlines(center1, max_height + 1, max_height + 3)
+        ax.vlines(center2, max_height + 1, max_height + 3)
+        ax.hlines(max_height + 3, center1, center2)
+        ax.text(np.mean([center1, center2]), max_height + 4, '*')
